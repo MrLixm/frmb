4d5399c2c45bffe9b639e08be44f6b2f713dcedb
Liam Collod <lcollod@gmail.com>
2024-02-13 20:31:42 +0100

    feat(parsing): add FrmbFormat.to_file()

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -175,3 +175,28 @@ def test_FrmbFormat_hash():
     assert hash(hierarchy_1) != hash(hierarchy_2)
     assert hash(hierarchy_2) == hash(hierarchy_3)
     assert hash(hierarchy_3) != hash(hierarchy_4)
+
+
+def test_FrmbFormat_tofile(data_dir, tmp_path):
+    # ensure to_file use the same logic as from_file
+    src_dir = data_dir / "structure1" / "studio"
+    hierarchy_src = read_menu_hierarchy(src_dir)
+
+    dst_path = tmp_path / "test1"
+    dst_path.mkdir()
+
+    for menu in hierarchy_src:
+        menu.to_file(dst_path)
+
+    hierarchy_dst = read_menu_hierarchy(dst_path)
+    assert hierarchy_dst == hierarchy_src
+
+    dst_path = tmp_path / "test2"
+    dst_path.mkdir()
+
+    for menu in hierarchy_src:
+        menu.to_file(dst_path, write_children=False)
+
+    hierarchy_dst = read_menu_hierarchy(dst_path)
+    assert hierarchy_dst != hierarchy_src
+    assert not any([path.is_dir() for path in dst_path.glob("*")])
